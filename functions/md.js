b412888b43468bb37a6dc544b8a86fb377939835
Vikas Ukani <vikas@maven.one>
2021-04-28 10:39:29 +0530

    Functions code updates.

---
+++ functions/md.js
@@ -0,0 +1,73 @@
+const defaults = {
+    types: {
+        standard: ['richtext'],
+        inline: ['string']
+    },
+    options: {
+        smartypants: true,
+        headerIds: false,
+        breaks: true
+    }
+}
+
+
+class StrapiMarkdown {
+    constructor(
+        model,
+        types = defaults.types,
+        options = defaults.options
+    ) {
+        if (model && model.attributes) {
+            this.model = model.attributes
+        } else {
+            throw new Error('`model` must be valid model object')
+        }
+        if (types && types.standard && Array.isArray(types.standard) && types.inline && Array.isArray(types.inline)) {
+            this.types = types
+        } else {
+            throw new Error('`types` must be object containing `standard` and `inline` arrays')
+        }
+        if (options && options.constructor === Object) {
+            this.marked = require('marked')
+            this.marked.setOptions(options)
+        } else {
+            throw new Error('`options` must be valid object')
+        }
+    }
+
+    parse = async data => {
+        try {
+            const item = await data
+
+            for (let key in this.model) {
+                if (item[key]) {
+                    if (this.types.standard.includes(this.model[key].type)) {
+                        item[key] = this.marked(item[key])
+                    } else if (this.types.inline.includes(this.model[key].type)) {
+                        item[key] = this.marked.parseInline(item[key])
+                    }
+                }
+            }
+            return item
+
+
+        } catch (error) {
+            console.error(error)
+
+        }
+    }
+    md = data => {
+        try {
+            if (Array.isArray(data)) {
+                return Promise.all(data.map(obj => this.parse(obj)))
+            } else {
+                return this.parse(data)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+}
+
+module.exports = StrapiMarkdown
