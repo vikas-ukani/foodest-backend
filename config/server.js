215fc367612f33308fb397c9c1da449268a33647
Vikas Ukani <vikas@maven.one>
2021-04-28 16:42:30 +0530

    Database Configuration and Function added.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b90ff06e956bfb987f617cd644be9efb'),
+    },
+  },
+});
